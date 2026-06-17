53c8e0c265b5621089e1d780c9ce0be1d415b66e
devms786178-lgtm <devms786178@gmail.com>
2026-06-17 13:39:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "8803215606:AAH_ZO2VV7ugQx1xEfQNrsusJkzqqfTGLH4"
+    BOT_TOKEN = "8492913128:AAFt3FNmqHf4zX17zt2-3FkTZhUr4LHikZM"
     # Telegram API ki ID
     API_ID = 38498066
     API_HASH = "c9696114751feacdeb1b4487f5839a1a"
